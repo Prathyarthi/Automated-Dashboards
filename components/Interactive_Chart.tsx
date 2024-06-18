be9daf2c9ca86de7492106acc307617598e126eb
Prathyarthi <prathyarti@gmail.com>
2024-06-18 23:30:36 +0530

    Added interactive chart with better UI

---
+++ components/Interactive_Chart.tsx
@@ -0,0 +1,220 @@
+"use client"
+
+import * as React from "react"
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+import { LabelList, Pie, PieChart } from "recharts"
+import { Area, AreaChart } from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export const description = "An interactive bar chart"
+
+const chartData = [
+    { date: "2024-04-01", desktop: 222, mobile: 150, fill: "hsl(var(--chart-1))" },
+    { date: "2024-04-02", desktop: 97, mobile: 180, fill: "hsl(var(--chart-2))" },
+    { date: "2024-04-03", desktop: 167, mobile: 120, fill: "hsl(var(--chart-3))" },
+    { date: "2024-04-04", desktop: 242, mobile: 260, fill: "hsl(var(--chart-4))" },
+    { date: "2024-04-05", desktop: 373, mobile: 290, fill: "hsl(var(--chart-5))" },
+]
+
+const chartConfig = {
+    views: {
+        label: "Page Views",
+    },
+    desktop: {
+        label: "Desktop",
+        color: "hsl(var(--chart-1))",
+    },
+    mobile: {
+        label: "Mobile",
+        color: "hsl(var(--chart-2))",
+    },
+} satisfies ChartConfig
+
+export function Interactive_Chart() {
+    const [activeChart, setActiveChart] =
+        React.useState<keyof typeof chartConfig>("desktop")
+
+    const total = React.useMemo(
+        () => ({
+            desktop: chartData.reduce((acc, curr) => acc + curr.desktop, 0),
+            mobile: chartData.reduce((acc, curr) => acc + curr.mobile, 0),
+        }),
+        []
+    )
+
+    return (
+        <Card>
+            <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
+                <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
+                    {/* <CardTitle>Bar Chart - Interactive</CardTitle>
+                    <CardDescription>
+                        Showing total visitors for the last 3 months
+                    </CardDescription> */}
+                </div>
+                <div className="flex">
+                    {["desktop", "mobile"].map((key) => {
+                        const chart = key as keyof typeof chartConfig
+                        return (
+                            <button
+                                key={chart}
+                                data-active={activeChart === chart}
+                                className="relative z-30 flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
+                                onClick={() => setActiveChart(chart)}
+                            >
+                                <span className="text-xs text-muted-foreground">
+                                    {chartConfig[chart].label}
+                                </span>
+                                <span className="text-lg font-bold leading-none sm:text-3xl">
+                                    {total[key as keyof typeof total].toLocaleString()}
+                                </span>
+                            </button>
+                        )
+                    })}
+                </div>
+            </CardHeader>
+            <CardContent className="px-2 sm:p-6">
+                <ChartContainer
+                    config={chartConfig}
+                    className="aspect-auto h-[250px] w-full"
+                >
+                    <BarChart
+                        accessibilityLayer
+                        data={chartData}
+                        margin={{
+                            left: 12,
+                            right: 12,
+                        }}
+                    >
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="date"
+                            tickLine={false}
+                            axisLine={false}
+                            tickMargin={8}
+                            minTickGap={32}
+                            tickFormatter={(value) => {
+                                const date = new Date(value)
+                                return date.toLocaleDateString("en-US", {
+                                    month: "short",
+                                    day: "numeric",
+                                })
+                            }}
+                        />
+                        <ChartTooltip
+                            content={
+                                <ChartTooltipContent
+                                    className="w-[150px]"
+                                    nameKey="views"
+                                    labelFormatter={(value) => {
+                                        return new Date(value).toLocaleDateString("en-US", {
+                                            month: "short",
+                                            day: "numeric",
+                                            year: "numeric",
+                                        })
+                                    }}
+                                />
+                            }
+                        />
+                        <Bar dataKey={activeChart} fill={`var(--color-${activeChart})`} />
+                    </BarChart>
+                </ChartContainer>
+                <ChartContainer
+                    config={chartConfig}
+                    className="mx-auto aspect-square max-h-[250px]"
+                >
+                    <PieChart>
+                        <ChartTooltip
+                            content={<ChartTooltipContent nameKey="visitors" hideLabel />}
+                        />
+                        <Pie data={chartData} dataKey={activeChart}>
+                            <LabelList
+                                dataKey="browser"
+                                className="fill-background"
+                                stroke="none"
+                                fontSize={12}
+                                formatter={(value: keyof typeof chartConfig) =>
+                                    chartConfig[value]?.label
+                                }
+                            />
+                        </Pie>
+                    </PieChart>
+                </ChartContainer>
+                <ChartContainer config={chartConfig}>
+                    <AreaChart
+                        accessibilityLayer
+                        data={chartData}
+                        margin={{
+                            left: 12,
+                            right: 12,
+                        }}
+                    >
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="month"
+                            tickLine={false}
+                            axisLine={false}
+                            tickMargin={8}
+                            tickFormatter={(value) => value.slice(0, 3)}
+                        />
+                        <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+                        <defs>
+                            <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                                <stop
+                                    offset="5%"
+                                    stopColor="var(--color-desktop)"
+                                    stopOpacity={0.8}
+                                />
+                                <stop
+                                    offset="95%"
+                                    stopColor="var(--color-desktop)"
+                                    stopOpacity={0.1}
+                                />
+                            </linearGradient>
+                            <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+                                <stop
+                                    offset="5%"
+                                    stopColor="var(--color-mobile)"
+                                    stopOpacity={0.8}
+                                />
+                                <stop
+                                    offset="95%"
+                                    stopColor="var(--color-mobile)"
+                                    stopOpacity={0.1}
+                                />
+                            </linearGradient>
+                        </defs>
+                        <Area
+                            dataKey="mobile"
+                            type="natural"
+                            fill="url(#fillMobile)"
+                            fillOpacity={0.4}
+                            stroke="var(--color-mobile)"
+                            stackId="a"
+                        />
+                        <Area
+                            dataKey="desktop"
+                            type="natural"
+                            fill="url(#fillDesktop)"
+                            fillOpacity={0.4}
+                            stroke="var(--color-desktop)"
+                            stackId="a"
+                        />
+                    </AreaChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
+    )
+}
