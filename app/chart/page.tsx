b8aba4506da292aca88a2f2459060c4e56c21ede
Prathyarthi <prathyarti@gmail.com>
2024-06-19 23:31:21 +0530

    Modified charts

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,26 +1,15 @@
-import { Bar_Chart } from "@/components/Bar_Chart"
+import { Interactive_Chart } from "@/components/Interactive_Chart"
 
 function page() {
-
-    const chartData = [
-        { month: "January", desktop: 186, mobile: 80 },
-        { month: "February", desktop: 305, mobile: 200 },
-        { month: "March", desktop: 237, mobile: 120 },
-        { month: "April", desktop: 73, mobile: 190 },
-        { month: "May", desktop: 209, mobile: 130 },
-        { month: "June", desktop: 214, mobile: 140 },
-    ]
 
     return (
         <main className="flex-1 p-2 lg:p-5 bg-gradient-to-bl from-white to-indigo-600 flex justify-center items-center">
             <div className="bg-white rounded-md drop-shadow-xl min-h-[90vh] max-h-[90vh] w-full p-2 grid grid-cols-12">
                 <div className="col-span-4">
-                    <div className="">
-                        <Bar_Chart chartData={chartData} />
-                    </div>
-                    <div className="">
-                        <Bar_Chart chartData={chartData} />
-                    </div>
+                    <Interactive_Chart />
+                </div>
+                <div>
+
                 </div>
             </div>
         </main>
--- components/Pie_Chart.tsx
+++ components/Pie_Chart.tsx
@@ -17,6 +17,7 @@ import {
     ChartTooltip,
     ChartTooltipContent,
 } from "@/components/ui/chart"
+
 const chartData = [
     { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
     { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
@@ -51,12 +52,12 @@ const chartConfig = {
     },
 } satisfies ChartConfig
 
-export function Component() {
+export function Pie_Chart() {
     return (
         <Card className="flex flex-col">
             <CardHeader className="items-center pb-0">
-                <CardTitle>Pie Chart - Label List</CardTitle>
-                <CardDescription>January - June 2024</CardDescription>
+                {/* <CardTitle>Pie Chart - Label List</CardTitle>
+                <CardDescription>January - June 2024</CardDescription> */}
             </CardHeader>
             <CardContent className="flex-1 pb-0">
                 <ChartContainer
@@ -67,7 +68,7 @@ export function Component() {
                         <ChartTooltip
                             content={<ChartTooltipContent nameKey="visitors" hideLabel />}
                         />
-                        <Pie data={chartData} dataKey="visitors">
+                        <Pie data={chartData} dataKey={"visitors"}>
                             <LabelList
                                 dataKey="browser"
                                 className="fill-background"
@@ -81,14 +82,14 @@ export function Component() {
                     </PieChart>
                 </ChartContainer>
             </CardContent>
-            <CardFooter className="flex-col gap-2 text-sm">
+            {/* <CardFooter className="flex-col gap-2 text-sm">
                 <div className="flex items-center gap-2 font-medium leading-none">
                     Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
                 </div>
                 <div className="leading-none text-muted-foreground">
                     Showing total visitors for the last 6 months
                 </div>
-            </CardFooter>
+            </CardFooter> */}
         </Card>
     )
 }
